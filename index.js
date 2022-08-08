670d8e0dfc84d04658bc8d1a80db3540fbb9f32e
Rodrigo Lezama <cordolezama@gmail.com>
2022-08-08 19:01:44 -0300

    Empezando e-commerce proyecto final JS

---
+++ index.js
@@ -0,0 +1,52 @@
+class Producto {
+    constructor (id,nombre,descripcion,precio){
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    precioConIva (){
+        return parseFloat((this.precio*IVA).toFixed(2));
+    }
+}
+
+const creoId = () => parseInt(Math.random()*10000)
+
+
+const crearProducto = () => {
+    productos.push (new Producto (creoId(), prompt("Indique el nombre del Producto"),prompt("Ingrese breve descripcion"),parseInt(prompt("Ingrese precio")))) 
+}
+
+const crearCarrito = () =>{
+    id = parseInt(prompt("Indique el ID de su producto para ingresarlo al carrito"));
+    carrito.push(productos.find(producto => producto.id === id));
+}
+
+
+// alert("atencion usted debe ingresar al menos 1 producto!");
+
+
+
+do {
+    crearProducto();
+    confirmacion = confirm ("Desea agregar un nuevo producto?")
+} while (confirmacion)
+
+productos.forEach(producto => {
+    console.table(producto);
+   });
+
+debugger
+do {
+    crearCarrito();
+    confirmacion = confirm ("Desea agregar otro producto al carrito")
+} while(confirmacion){
+    carrito.forEach(producto => {
+        console.table(producto);
+        total =+ producto.precioConIva();
+    });
+    console.warn(`El total a pagar es de: ${total}`)
+}
+
+
---
+++ variables.js
@@ -0,0 +1,7 @@
+const productos = [];
+const carrito = [];
+const IVA = 1.21;
+
+let confirmacion;
+let total = 0;
+let id = 0;
