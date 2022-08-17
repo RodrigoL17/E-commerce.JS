be7274af61a2db609162a62f671307d651a28640
Rodrigo Lezama <cordolezama@gmail.com>
2022-08-17 20:11:46 -0300

    lala

--- index.js
+++ index.js
@@ -14,69 +14,62 @@ class Producto {
 
 const creoId = () => parseInt(Math.random() * 10000);
 
-const nombre = document.getElementById("obtener-nombre");
-const precio = document.getElementById("obtener-precio");
-const codigo = document.getElementById("obtener-codigo");
-const descripcion = document.getElementById("obtener-descripcion")
-
-const btnNuevoProducto = document.getElementById("btn-nuevo-producto");
-const btnAgregar = document.getElementById("btn-agregar")
-
-const nuevoProducto = () => {  
+const nuevoProducto = () => { 
+  debugger 
   codigo.value = creoId();
   nombre.value = "";
   descripcion.value = "";
-  nombre.focus();
-};
+  // nombre.focus();
+}
 
 const guardarProductos = () => {
   productos.push(new Producto(codigo.value,nombre.value,descripcion.value,precio.value));
   localStorage.setItem("productos", JSON.stringify(productos));
 }
 
-btnAgregar.addEventListener("click", guardarProductos);
+
 btnNuevoProducto.addEventListener("click", nuevoProducto);
+btnAgregar.addEventListener("click", guardarProductos);
+
 
 const recuperarDeLS = () =>{
   if (localStorage.productos){
   const prodGuardados = JSON.parse(localStorage.getItem("productos"));
     prodGuardados.forEach(prod => {
       productos.push(prod)
+      console.log(productos)
     })
   }
 }
 
-const crearTarjeta = (producto) => {
-  const tarjeta = document.createElement("div");
-  tarjeta.innerHTML = `<div class="card" style="width: 15rem">
-                          <img src="..." class="card-img-top" alt="..." />
-                          <div class="card-body">
-                            <h4 class="card-title">
-                              ${producto.nombre}
-                            </h4>
-                            <h5 class="card-title">
-                              ${producto.id}
-                            </h5>
-                            <p class="card-text">
-                              ${producto.descripcion}
-                            </p>
-                            <h5 class="card-title">
-                              ${producto.precio}
-                            </h5>
-                            <button class="btn btn-outline-dark" type="submit">Agregar al carrito</button>
-                          </div>
-                        </div>`;
-  return tarjeta;
-};
-
-function listarProductos () {
-  const lista = document.getElementById("cuerpo");
+const listarProductos = () => {
   recuperarDeLS();
+  console.log(listaProductos)
+  if (!listaProductos) {return null}
+  listaProductos.innerHTML = "";
   productos.forEach(producto => {
-    lista.appendChild(crearTarjeta(producto));
-  });
+  listaProductos.innerHTML += `<div class="card" style="width: 15rem">
+                                    <img src="..." class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                      <h4 class="card-title">
+                                        ${producto.nombre}
+                                      </h4>
+                                      <h5 class="card-title">
+                                        ${producto.id}
+                                      </h5>
+                                      <p class="card-text">
+                                        ${producto.descripcion}
+                                      </p>
+                                      <h5 class="card-title">
+                                        ${producto.precio}
+                                      </h5>
+                                      <button class="btn btn-outline-dark" type="submit">Agregar al carrito</button>
+                                    </div>
+                                  </div>`;
+  })
 };
 
+listarProductos();
 
 
 // const crearCarrito = () => {
--- variables.js
+++ variables.js
@@ -4,3 +4,13 @@ const IVA = 1.21;
 
 let confirmacion;
 let total = 0;
+
+const nombre = document.getElementById("obtener-nombre");
+const precio = document.getElementById("obtener-precio");
+const codigo = document.getElementById("obtener-codigo");
+const descripcion = document.getElementById("obtener-descripcion")
+
+const listaProductos = document.querySelector("#lista-productos")
+
+const btnNuevoProducto = document.querySelector("#btn-nuevo-producto");
+const btnAgregar = document.querySelector("#btn-agregar")
